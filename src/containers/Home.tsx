4d636b5638919e451da339121ac21eaa11599db7
sreerenjini-keyvalue <sreerenjini@keyvalue.systems>
2022-08-27 14:35:59 +0530

    Add Carousel component

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,33 +1,15 @@
 /* eslint-disable max-len */
-import Cards from '@components/cards/Cards';
-import Map from '@components/map/map';
 import React from 'react';
 
-const imgArray = [
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-  'https://www.worldtrips.com/-/media/Project/TMHCC-MIS/HCCMISdotcom/Blog-Posts/travel/backpacker-wading-through-a-river-with-his-packjpg.jpg',
-];
+import Carousel from '@components/carousel/Carousel';
+import Map from '@components/map/map';
+
 const Home = () => {
   return (
-    <div className='w-full h-full bg-white'>
+    <div className="w-full h-full bg-white">
       Home page
-      <div className='flex overflow-auto flex-row m-3 customTransparentScroll'>
-        {imgArray.map((imgUlr, index) => (
-          <div key={index} className="m-2">
-            <Cards img={imgUlr} />
-          </div>
-        ))}
-      </div>
+      <Carousel />
+      <div className="flex overflow-auto flex-row m-3 customTransparentScroll"></div>
       <Map />
     </div>
   );
