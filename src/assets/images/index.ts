4932b4c98cbb99b5c256d100960ad4872982b770
cizil99 <cizil@keyvalue.systems>
2022-08-28 04:31:02 +0530

    Header

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,6 @@
 import EmptyPortfolio from './EmptyPortfolio.png';
 import NoResultFound from './NoResultFound.png';
 import NoTransactions from './NoTransactions.png';
+import Profile from './profile.png';
 
-export { EmptyPortfolio, NoResultFound, NoTransactions };
+export { EmptyPortfolio, NoResultFound, NoTransactions , Profile};
