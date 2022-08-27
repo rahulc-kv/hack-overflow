23e28522c36a566750bfc3e4fc3aa37876e6fb2b
rahulc97 <rahul.c@keyvalue.systems>
2022-08-27 14:09:25 +0530

    mapbox integrated

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,10 +2,12 @@ import app from './appReducer';
 import leftNavigation from './leftNavigation';
 import userDetails from './userDetails';
 import userAuth from './userAuth';
+import eventsReducer from './eventsReducer';
 
 export default {
   app,
   leftNavigation,
   userDetails,
-  userAuth
+  userAuth,
+  eventsReducer
 };
