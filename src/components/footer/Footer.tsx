f02ee1336b26157ffcc8f881c241c6d9a2ea1c14
Shanimol E M <shanimol@keyvalue.systems>
2022-08-27 20:22:54 +0530

    Added feedCard

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      <div className=" fixed bottom-0 w-full border-b border-gray-200 dark:border-gray-700">
-        <div className="w-full flex flex-col h-12 "></div>
+      <div className="fixed bottom-0 mt-10 w-full border-b border-gray-200 dark:border-gray-700">
+        <div className="flex flex-col w-full h-12 "></div>
         <footer
           className="fixed
              inset-x-0 bottom-0 
              text-3xl 
              text-white
    
-             border-t-1
-             border-gray-500">
+             border-gray-500
+             border-t-1">
           <button className="w-1/4 h-12 text-sm font-bold text-white bg-gray-300  hover:border-gray-400 outline-none shadow-sm">
             <div className="flex gap-x-1 justify-center items-center align-center">
               Feed
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 
 import Carousel from '@components/carousel/Carousel';
 import Footer from '@components/footer/Footer';
-import Map from '@components/map/map';
+// import Map from '@components/map/map';
+import FeedCard from '@components/feed-card/FeedCard';
 
 const Home = () => {
   return (
-    <div className="w-full h-full bg-white">
+    <div className="overflow-auto w-full h-full bg-white customTransparentScroll">
       Home page
       <Carousel />
-      <div className="flex overflow-auto flex-row m-3 customTransparentScroll"></div>
-      <Map />
+      <div className="flex w-full h-auto">
+        <FeedCard img="https://cdn.pixabay.com/photo/2022/03/20/15/40/nature-7081138__340.jpg" />
+      </div>
+      {/* <div className="flex overflow-auto flex-row customTransparentScroll">
+
+      </div> */}
+      {/* <Map /> */}
       <Footer />
     </div>
   );
