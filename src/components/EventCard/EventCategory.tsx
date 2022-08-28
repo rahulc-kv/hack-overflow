a31916b8f4c922fb918835eeedcf03ee13daf0a7
sreerenjini-keyvalue <sreerenjini@keyvalue.systems>
2022-08-28 07:53:46 +0530

    Handle redirection from eventcard

--- src/components/EventCard/EventCategory.tsx
+++ src/components/EventCard/EventCategory.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 const EventCategory = ({ category }) => {
+  const navigate = useNavigate();
+  const handleEventCardClick = () => {
+    navigate('/explore');
+  };
 
   return (
 
-    <div className={'flex overflow-hidden  flex-col mr-3  w-52 h-[200px] bg-white rounded-lg shadow-xl'}>
+    <div className={'flex overflow-hidden  flex-col mr-3  w-52 h-[200px] bg-white rounded-lg shadow-xl'} role={'presentation'} onClick={handleEventCardClick}>
       <img
         className="w-auto h-auto rounded rounded-b-none opacity-80"
         src={category?.image}
