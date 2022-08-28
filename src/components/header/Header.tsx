0b346dea2c4a466f3ebbd140d4f92ee83b675db1
rahulc97 <rahul.c@keyvalue.systems>
2022-08-28 07:11:58 +0530

    logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,19 +7,20 @@ const Header = () => {
         <div
             className="flex relative top-0 z-50  px-2 pt-3 pb-2
        w-full h-14 font-inter bg-white border-b-[1px] border-[#E5E7EB] ">
-            <div className="flex absolute left-4 flex-row">
+            <img className='absolute left-[150px] h-[38px]' src="/images/whn.png" />
+            <div className="flex absolute left-[10px] flex-row">
                 <div className=" p-1 h-9 bg-gray-100 rounded-lg">
                     <LocationIcon
                         className="mt-1 h-[19px]"
                     />
                 </div>
                 <div className="flex flex-col pt-1 pl-2">
-                <div className="text-xs">Edachira</div>
-                <div className="text-[10px]">Kakkanad</div>
+                    <div className="text-xs">Edachira</div>
+                    <div className="text-[10px]">Kakkanad</div>
                 </div>
             </div>
             <div className="flex absolute right-3 flex-row">
-              
+
                 <div className="mt-1"><SettingsIcon fill="#000000" className=" mr-2 w-7 h-7 " /> </div>
 
             </div>
